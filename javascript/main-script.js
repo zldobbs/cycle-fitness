e25db8e61c22ba8e99fb7c0364afb3298bd038d8
Zach <dobbszach@gmail.com>
2017-09-07 21:56:31 -0500

    Initial Commit - get on GitHub

---
+++ javascript/main-script.js
@@ -0,0 +1,7 @@
+// Javascript Document
+// Loads background image and content on page
+$(document).ready(function() {
+    $("#page_content").fadeIn(2000);
+    $("li").hide(0).fadeIn(2000);
+    $("#load_back").delay(800).fadeIn(2000);
+});
